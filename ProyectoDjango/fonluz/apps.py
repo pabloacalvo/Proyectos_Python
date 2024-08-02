9ca4b3bb3b7228972fd4e5c15103e21d7fe1f57a
pc250282 <pablo.calvo@ncr.com>
2024-08-01 22:03:53 -0300

    delete

--- ProyectoDjango/fonluz/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class FonluzConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fonluz'
-    verbose_name = 'Gestion de productos'
